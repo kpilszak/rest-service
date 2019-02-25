e0155a42f226b7afa0f9dc30776f900bf9c51002
kpilszak <klaudia.pilszak@gmail.com>
2019-02-25 21:28:48 +0100

    Add helloWorld()

--- src/main/java/com/projects/restservice/HelloWorldController.java
+++ src/main/java/com/projects/restservice/HelloWorldController.java
@@ -1,4 +1,11 @@
 package com.projects.restservice;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+
+    public String helloWorld() {
+        return "Hello world";
+    }
 }
