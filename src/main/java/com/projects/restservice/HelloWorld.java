14d8356a6a82eb63a9975a51d1aea9571400ad19
kpilszak <klaudia.pilszak@gmail.com>
2019-02-25 22:22:58 +0100

    Create HelloWorld.java

---
+++ src/main/java/com/projects/restservice/HelloWorld.java
@@ -0,0 +1,36 @@
+package com.projects.restservice;
+
+public class HelloWorld {
+
+    private String message;
+    private String name;
+
+    public HelloWorld(String message, String name) {
+        this.message = message;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorld{" +
+                "message='" + message + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
