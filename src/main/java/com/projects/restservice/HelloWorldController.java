a726d618f23dc873e4a40796c778fbb11fd8fd9f
kpilszak <klaudia.pilszak@gmail.com>
2019-02-25 21:42:35 +0100

    Add name parameter using PathVariable

--- src/main/java/com/projects/restservice/HelloWorldController.java
+++ src/main/java/com/projects/restservice/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.projects.restservice;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/hello")
-    public String helloWorld() {
-        return "Hello world";
+    @RequestMapping(method = RequestMethod.GET, path = "/hello/(name)")
+    public String helloWorld(@PathVariable String name) {
+        return String.format("Hello @s", name);
     }
 }
