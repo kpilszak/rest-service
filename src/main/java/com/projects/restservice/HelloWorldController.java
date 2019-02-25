4a953772a87784f783828754517cb27694711e6e
kpilszak <klaudia.pilszak@gmail.com>
2019-02-25 22:17:51 +0100

    Do RequestMapping with PathParam instead of PathVariable

--- src/main/java/com/projects/restservice/HelloWorldController.java
+++ src/main/java/com/projects/restservice/HelloWorldController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
+
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/hello/{name}")
-    public String helloWorld(@PathVariable String name) {
+    @RequestMapping(method = RequestMethod.GET, path = "/hello")
+    public String helloWorld(@PathParam("name") String name) {
         return String.format("Hello %s", name);
     }
 }
