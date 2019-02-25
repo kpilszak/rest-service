054f79708c628568b7c017f830b8c2a7c1c2d969
kpilszak <klaudia.pilszak@gmail.com>
2019-02-25 21:30:33 +0100

    Add @RequestMapping

--- src/main/java/com/projects/restservice/HelloWorldController.java
+++ src/main/java/com/projects/restservice/HelloWorldController.java
@@ -1,10 +1,13 @@
 package com.projects.restservice;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    @RequestMapping(method = RequestMethod.GET, path = "/hello")
     public String helloWorld() {
         return "Hello world";
     }
